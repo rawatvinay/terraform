0283483b39931d14ee59b9e9694dbbc19f2b47ff
rawatvinay <54552338+rawatvinay@users.noreply.github.com>
2022-08-17 13:26:46 +0530

    Update main.tf

--- env/sandbox/appserver/main.tf
+++ env/sandbox/appserver/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = $AWS_ACCESS_KEY_ID
-  secret_key = $AWS_SECRET_ACCESS_KEY
+  access_key = "$AWS_ACCESS_KEY_ID"
+  secret_key = "$AWS_SECRET_ACCESS_KEY"
   region = "ap-south-1"
 }
 
